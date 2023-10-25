46f083536e9bcd2bb55f1c1163d7302e985ba723
Sara Regan <1307818+sareg0@users.noreply.github.com>
2023-10-25 09:02:48 +0200

    feat: add cart count to main page (#3)
    * feat: add cart count to main page
    
    * chore: remove some comments and files that shouldn't be checked in

--- src/stores/cart.spec.ts
+++ src/stores/cart.spec.ts
@@ -2,6 +2,9 @@ import { setActivePinia, createPinia } from "pinia";
 import { useCartStore } from "./cart";
 import { describe, beforeEach, it, expect } from "vitest";
 
+const uuid_one = "94ddf238-421a-4a51-a8c9-341280d9f0de";
+const uuid_two = "d2171b60-7cff-41fb-9fca-5c5125f38055";
+
 describe("Cart Store", () => {
   // does the afterEach in my vitest.setup.ts take care of this?
   beforeEach(() => {
@@ -10,20 +13,47 @@ describe("Cart Store", () => {
     // without having to pass it to it: `useStore(pinia)`
     setActivePinia(createPinia());
   });
-  it("adds a given product to the cart", () => {
-    const cart = useCartStore();
-    expect(cart.items.length).toBe(0);
-    cart.addToCart("1234-5678");
-    expect(cart.items.length).toBe(1);
-    cart.addToCart("1234-5678");
+  describe("Getters", () => {
+    describe("getItemByID", () => {
+      it("returns a given item", () => {
+        const cart = useCartStore();
+        expect(cart.getItemByID(uuid_one)).toBeUndefined();
+        expect(cart.getItemByID(uuid_two)).toBeUndefined();
+        cart.addToCart(uuid_one);
+        expect(cart.getItemByID(uuid_one)).toMatchObject({
+          id: uuid_one,
+          count: 1,
+        });
+        expect(cart.getItemByID(uuid_two)).toBeUndefined();
+      });
+    });
+    describe("getTotalCount", () => {
+      it("returns the total count of the items in the cart", () => {
+        const cart = useCartStore();
+        cart.addToCart(uuid_one);
+        cart.addToCart(uuid_one);
+        cart.addToCart(uuid_two);
+        expect(cart.items.length).toBe(2);
+        expect(cart.getTotalCount).toBe(3);
+      });
+    });
   });
-  it("incremenets an item that is already in the cart", () => {
-    const cart = useCartStore();
-    expect(cart.items.length).toBe(0);
-    const uuid = "1234-5678";
-    cart.addToCart(uuid);
-    cart.addToCart(uuid);
-    expect(cart.items.length).toBe(1);
-    expect(cart.getItemByID(uuid)).toMatchObject({ count: 2 });
+  describe("Actions", () => {
+    it("adds a given product to the cart", () => {
+      const cart = useCartStore();
+      expect(cart.items.length).toBe(0);
+      cart.addToCart("1234-5678");
+      expect(cart.items.length).toBe(1);
+      cart.addToCart("1234-5678");
+    });
+    it("incremenets an item that is already in the cart", () => {
+      const cart = useCartStore();
+      expect(cart.items.length).toBe(0);
+      const uuid = "1234-5678";
+      cart.addToCart(uuid);
+      cart.addToCart(uuid);
+      expect(cart.items.length).toBe(1);
+      expect(cart.getItemByID(uuid)).toMatchObject({ count: 2 });
+    });
   });
 });
--- src/views/ProductListingView.vue
+++ src/views/ProductListingView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="listing-grid">
+    <h3 v-if="products.loading">loading products...</h3>
+    <h3 v-if="products.error">error while loading products...</h3>
     <ProductItem
-      v-for="product in products"
+      v-else
+      @add="handleAddToCart"
+      v-for="product in products.data"
       :id="product.id"
       :name="product.name"
       :image="product.image"
@@ -11,33 +15,66 @@
 </template>
 
 <script setup lang="ts">
-import ProductItem from "@/components/ProductItem/ProductItem.vue";
-const products = [
-  {
-    id: "510e1188-b092-437a-aff1-050b35bc57e7",
-    name: "Cosmic Y",
-    description: "A description of Cosmic Y",
-    image: "cosmicy-1.png",
-  },
-  {
-    id: "32367aff-485e-48f9-813e-a69914041e70",
-    name: "Cosmic Y",
-    description: "A description of Cosmic Y",
-    image: "cosmicy-1.png",
-  },
-  {
-    id: "46f7c2cf-32a7-4902-a66e-7037be700651",
-    name: "Cosmic Y",
-    description: "A description of Cosmic Y",
-    image: "cosmicy-1.png",
-  },
-  {
-    id: "f753169d-3f07-4570-9593-db0bb4ec30a9",
-    name: "Cosmic Y",
-    description: "A description of Cosmic Y",
-    image: "cosmicy-1.png",
-  },
-];
+import { useCartStore } from "@/stores/cart";
+import { ref, onMounted } from "vue";
+
+import ProductItem, {
+  type Props as ProductItemType,
+} from "@/components/ProductItem/ProductItem.vue";
+import { version } from "os";
+
+const store = useCartStore();
+
+const handleAddToCart = (item: { id: string }) => {
+  store.addToCart(item.id);
+};
+
+const products = ref<{
+  loading: boolean;
+  data: ProductItemType[] | undefined;
+  error: boolean;
+}>({
+  loading: true,
+  data: undefined,
+  error: false,
+});
+
+// ToDo: improve data fetching
+// How do I test a component like this?
+// I could test that the timer is run down for this fake version, but what would happen when it is no longer
+// implemented like this?
+
+onMounted(() => {
+  setTimeout(() => {
+    products.value.data = [
+      {
+        id: "510e1188-b092-437a-aff1-050b35bc57e7",
+        name: "Cosmic Y",
+        image: "cosmicy-1.png",
+        description: "A description of Cosmic Y",
+      },
+      {
+        id: "32367aff-485e-48f9-813e-a69914041e70",
+        name: "Cosmic Y",
+        image: "cosmicy-1.png",
+        description: "A description of Cosmic Y",
+      },
+      {
+        id: "46f7c2cf-32a7-4902-a66e-7037be700651",
+        name: "Cosmic Y",
+        image: "cosmicy-1.png",
+        description: "A description of Cosmic Y",
+      },
+      {
+        id: "f753169d-3f07-4570-9593-db0bb4ec30a9",
+        name: "Cosmic Y",
+        image: "cosmicy-1.png",
+        description: "A description of Cosmic Y",
+      },
+    ];
+    products.value.loading = false;
+  }, 2000);
+});
 </script>
 
 <style scoped>
